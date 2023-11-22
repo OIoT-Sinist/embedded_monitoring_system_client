308fa6ad825cc16347a77d420c7ae1e805600664
Kanomace <3065566279@qq.com>
2023-11-22 16:54:10 +0800

    update mainactivity_structure

---
+++ android/app/src/main/java/com/openIoT/client/ui/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.openIoT.client.ui.activities
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+    }
+}
