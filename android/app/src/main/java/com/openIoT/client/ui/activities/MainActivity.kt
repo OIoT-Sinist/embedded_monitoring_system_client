9e611fa8af120bb12295e99ad22350efc939c31e
utf8-coding <1422346447@qq.com>
2023-11-22 19:09:19 +0800

    Adding viewmodel to one of the activities

--- android/app/src/main/java/com/openIoT/client/ui/activities/MainActivity.kt
+++ android/app/src/main/java/com/openIoT/client/ui/activities/MainActivity.kt
@@ -5,8 +5,13 @@ import android.os.PersistableBundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 
+lateinit var mainActivityViewModel: MainActivityViewModel;
 class MainActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
+        mainActivityViewModel = setViewModel(this)
+        setContent{
+            MainActivityView(mainActivityViewModel)
+        }
     }
 }
