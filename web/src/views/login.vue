71c4f6c7c29bd15417e006adc6cf5403aa8156e5
xujiasen163 <119720331+xujiasen163@users.noreply.github.com>
2023-12-06 12:22:15 +0800

    views

--- web/src/views/login.vue
+++ web/src/views/login.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="login">
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form">
-      <h3 class="title">RuoYi-Vue-Plus后台管理系统</h3>
+      <h3 class="title">设备管理系统</h3>
       <el-form-item prop="username">
         <el-input
           v-model="loginForm.username"
@@ -56,7 +56,6 @@
     </el-form>
     <!--  底部  -->
     <div class="el-login-footer">
-      <span>Copyright © 2018-2023 疯狂的狮子Li All Rights Reserved.</span>
     </div>
   </div>
 </template>
@@ -161,7 +160,7 @@ export default {
   justify-content: center;
   align-items: center;
   height: 100%;
-  background-image: url("../assets/images/login-background.jpg");
+  background-image: url("../assets/images/background.png");
   background-size: cover;
 }
 .title {
